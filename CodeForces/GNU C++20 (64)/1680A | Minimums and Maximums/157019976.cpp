354fdcf486b89c6f7f9e31b962419fd045183a17
trieu9797 <72329092+trieu9797@users.noreply.github.com>
2022-08-31 19:53:52 +0700

    Committing files

---
+++ CodeForces/GNU C++20 (64)/1680A | Minimums and Maximums/157019976.cpp
@@ -0,0 +1,40 @@
+/*
+Author : DeMen100ns (a.k.a Vo Khac Trieu)
+School : VNU-HCM High school for the Gifted
+fuck you adhoc
+*/
+ 
+#include <bits/stdc++.h>
+ 
+using namespace std;
+ 
+const int N = 2e5 + 5;
+const long long INF = 1e18 + 7;
+const int MAXA = 1e9;
+const int B = sqrt(N) + 5;
+ 
+void solve()
+{
+    int l1, r1, l2, r2;
+    cin >> l1 >> r1 >> l2 >> r2;
+    int ans = l1 + l2;
+    if (l1 <= r2 && l1 >= l2) ans = min(ans, l1);
+    if (l2 <= r1 && l2 >= l1) ans = min(ans, l2);
+    cout << ans << endl;
+}
+ 
+signed main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+ 
+    // freopen("codeforces.inp","r",stdin);
+    // freopen("codeforces.out","w",stdout);
+ 
+    int t = 1; cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+}
