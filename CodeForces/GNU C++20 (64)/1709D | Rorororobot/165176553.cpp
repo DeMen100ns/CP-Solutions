00f06092f6d9341779237bf983596e5137d6af8d
trieu9797 <72329092+trieu9797@users.noreply.github.com>
2022-08-31 19:49:51 +0700

    Committing files

---
+++ CodeForces/GNU C++20 (64)/1709D | Rorororobot/165176553.cpp
@@ -0,0 +1,225 @@
+/*
+Author : DeMen100ns (a.k.a Vo Khac Trieu)
+School : VNU-HCM High school for the Gifted
+fuck you adhoc
+*/
+ 
+#include <bits/stdc++.h>
+ 
+#include <algorithm>
+#include <cassert>
+#include <vector>
+ 
+ 
+#ifdef _MSC_VER
+#include <intrin.h>
+#endif
+ 
+namespace atcoder {
+ 
+namespace internal {
+ 
+int ceil_pow2(int n) {
+    int x = 0;
+    while ((1U << x) < (unsigned int)(n)) x++;
+    return x;
+}
+ 
+constexpr int bsf_constexpr(unsigned int n) {
+    int x = 0;
+    while (!(n & (1 << x))) x++;
+    return x;
+}
+ 
+int bsf(unsigned int n) {
+#ifdef _MSC_VER
+    unsigned long index;
+    _BitScanForward(&index, n);
+    return index;
+#else
+    return __builtin_ctz(n);
+#endif
+}
+ 
+}  // namespace internal
+ 
+}  // namespace atcoder
+ 
+ 
+namespace atcoder {
+ 
+template <class S, S (*op)(S, S), S (*e)()> struct segtree {
+  public:
+    segtree() : segtree(0) {}
+    explicit segtree(int n) : segtree(std::vector<S>(n, e())) {}
+    explicit segtree(const std::vector<S>& v) : _n(int(v.size())) {
+        log = internal::ceil_pow2(_n);
+        size = 1 << log;
+        d = std::vector<S>(2 * size, e());
+        for (int i = 0; i < _n; i++) d[size + i] = v[i];
+        for (int i = size - 1; i >= 1; i--) {
+            update(i);
+        }
+    }
+ 
+    void set(int p, S x) {
+        assert(0 <= p && p < _n);
+        p += size;
+        d[p] = x;
+        for (int i = 1; i <= log; i++) update(p >> i);
+    }
+ 
+    S get(int p) const {
+        assert(0 <= p && p < _n);
+        return d[p + size];
+    }
+ 
+    S prod(int l, int r) const {
+        assert(0 <= l && l <= r && r <= _n);
+        S sml = e(), smr = e();
+        l += size;
+        r += size;
+ 
+        while (l < r) {
+            if (l & 1) sml = op(sml, d[l++]);
+            if (r & 1) smr = op(d[--r], smr);
+            l >>= 1;
+            r >>= 1;
+        }
+        return op(sml, smr);
+    }
+ 
+    S all_prod() const { return d[1]; }
+ 
+    template <bool (*f)(S)> int max_right(int l) const {
+        return max_right(l, [](S x) { return f(x); });
+    }
+    template <class F> int max_right(int l, F f) const {
+        assert(0 <= l && l <= _n);
+        assert(f(e()));
+        if (l == _n) return _n;
+        l += size;
+        S sm = e();
+        do {
+            while (l % 2 == 0) l >>= 1;
+            if (!f(op(sm, d[l]))) {
+                while (l < size) {
+                    l = (2 * l);
+                    if (f(op(sm, d[l]))) {
+                        sm = op(sm, d[l]);
+                        l++;
+                    }
+                }
+                return l - size;
+            }
+            sm = op(sm, d[l]);
+            l++;
+        } while ((l & -l) != l);
+        return _n;
+    }
+ 
+    template <bool (*f)(S)> int min_left(int r) const {
+        return min_left(r, [](S x) { return f(x); });
+    }
+    template <class F> int min_left(int r, F f) const {
+        assert(0 <= r && r <= _n);
+        assert(f(e()));
+        if (r == 0) return 0;
+        r += size;
+        S sm = e();
+        do {
+            r--;
+            while (r > 1 && (r % 2)) r >>= 1;
+            if (!f(op(d[r], sm))) {
+                while (r < size) {
+                    r = (2 * r + 1);
+                    if (f(op(d[r], sm))) {
+                        sm = op(d[r], sm);
+                        r--;
+                    }
+                }
+                return r + 1 - size;
+            }
+            sm = op(d[r], sm);
+        } while ((r & -r) != r);
+        return 0;
+    }
+ 
+  private:
+    int _n, size, log;
+    std::vector<S> d;
+ 
+    void update(int k) { d[k] = op(d[2 * k], d[2 * k + 1]); }
+};
+ 
+}  // namespace atcoder
+ 
+ 
+using namespace std;
+using namespace atcoder;
+ 
+#define ii pair<int, int>
+#define x first
+#define y second
+#define int long long
+ 
+const int N = 2e5 + 5;
+const long long INF = 1e18 + 7;
+const int MAXA = 1e9;
+const int B = sqrt(N) + 5;
+ 
+int op(int a, int b) {
+    return max(a, b);
+}
+ 
+int e() {
+    return -1;
+}
+ 
+segtree<int, op, e> seg(N);
+int a[N];
+ 
+void solve()
+{
+    int n, m; cin >> n >> m;
+    for(int i = 1; i <= m; ++i){
+        cin >> a[i];
+        seg.set(i, a[i]);
+    }
+    int q; cin >> q;
+ 
+    while (q--){
+        ii s, t; int k;
+        cin >> s.x >> s.y >> t.x >> t.y >> k;
+        if ((s.x - t.x) % k || (s.y - t.y) % k){
+            cout << "NO" << endl;
+            continue;
+        }
+        int p = (n - s.x) / k, ma;
+        s.x += p * k;
+        if (s.y < t.y){
+            ma = seg.prod(s.y + 1, t.y + 1);
+        } else {
+            ma = seg.prod(t.y, s.y);
+        }
+        if (ma < s.x){
+            cout << "YES" << endl;
+        } else cout << "NO" << endl;
+    }
+}
+ 
+signed main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+ 
+    // freopen("codeforces.inp","r",stdin);
+    // freopen("codeforces.out","w",stdout);
+ 
+    int t = 1; // cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+}
