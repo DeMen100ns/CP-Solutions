9c0a8fd6789ae123d57c6b4a936456ba0f292a80
trieu9797 <72329092+trieu9797@users.noreply.github.com>
2022-08-31 19:51:41 +0700

    Committing files

---
+++ CodeForces/GNU C++20 (64)/1697A | Parkway Walk/160280091.cpp
@@ -0,0 +1,41 @@
+/*
+Author : DeMen100ns (a.k.a Vo Khac Trieu)
+School : VNU-HCM High school for the Gifted
+fuck you adhoc
+*/
+ 
+#include <bits/stdc++.h>
+ 
+using namespace std;
+ 
+const int N = 2e5 + 5;
+const long long INF = 1e18 + 7;
+const int MAXA = 1e9;
+const int B = sqrt(N) + 5;
+ 
+int a[N];
+ 
+void solve()
+{
+    int n, m; cin >> n >> m;
+    for(int i = 1; i <= n; ++i) cin >> a[i], m -= a[i];
+    m = -m;
+    if (m < 0) m = 0;
+    cout << m << endl;
+}
+ 
+signed main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+ 
+    // freopen("codeforces.inp","r",stdin);
+    // freopen("codeforces.out","w",stdout);
+ 
+    int t = 1; cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+}
