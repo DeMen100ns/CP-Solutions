67e125a43be3ca73f3025602ba05c50c3ecc9308
trieu9797 <72329092+trieu9797@users.noreply.github.com>
2022-08-31 19:45:03 +0700

    Committing files

---
+++ Atcoder/C++ (GCC 9.2.1)/abc188_d/19337591.cpp
@@ -0,0 +1,98 @@
+//Author : Trieu self-destruct :p
+//TST next year
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+/*Weird define*/
+#define ll long long
+#define endl '\n'
+#define gcd __gcd
+#define p prev
+
+/*For define*/
+#define For(i,a,b) for(ll i=a;i<=b;i++)
+#define foR(i,a,b) for(ll i=a;i>=b;i--)
+#define FOR(i,a,b) for(ll i=a;i<b;i++)
+#define forN(i,a,b,c) for(ll i=a;i<=b;i+=c)
+#define foRN(i,a,b,c) for(ll i=a;i>=b;i-=c)
+#define fora(i, a) for (auto i : a)
+
+/*push-pos define*/
+#define pb push_back
+#define pob pop_back
+#define pf push_front
+#define pof pop_front
+#define x first
+#define y second
+
+/*Prll define*/
+#define write(a, l, r) for (ll ik = l; ik <= r; ik++) cout << a[ik] << ' '; cout << endl
+#define writeln(a, l, r) for (ll ik = l; ik <= r; ik++) cout << a[ik] << endl
+
+/*Type-def :p*/
+typedef unsigned long long 	ull;
+typedef vector<ll> 		vi;
+typedef pair<ll, ll>		ii;
+typedef long double         ld;
+
+/*some const*/
+const ll IMA = INT_MAX;
+const ll IMI = INT_MIN;
+const ll MOD = 274876858367;
+
+/* Calculation with MODULO*/
+ll dec(ll x, ll y) {return (x-y+MOD)%MOD;}
+ll add(ll x, ll y) {return (x + y) % MOD;}
+ll mul(ll x, ll y) {return ((x%MOD) * (y%MOD)) % MOD;}
+ll bpow(ll x, ll y) {ll res = 1; while (y) {if (y & 1) res = mul(res, x); x = mul(x, x); y >>= 1;} return res;}
+ll ModInverse(ll x) {return bpow(x, MOD - 2);}
+ll Div(ll x, ll y) {return mul(x, ModInverse(y));}
+
+/*Some simple checking and calculation function*/
+string yn(bool k){if (k) return "YES"; else return "NO";}
+ll binpower(ll base, ll e, ll mod) {ll result = 1;base %= mod;while (e){if (e & 1)result = (ll)result * base % mod;base = (ll)base * base % mod;e >>= 1;    }return result;}
+bool check_composite(long long n, long long a, long long d, int s) {long long x = binpower(a, d, n); if (x == 1 || x == n - 1)return false;for (int r = 1; r < s; r++) {x = (long long)x * x % n;if (x == n - 1)return false;}return true;};
+bool checkprime(long long n) {if (n < 2)return false;int r = 0;long long d = n - 1;while ((d & 1) == 0) {d >>= 1;r++;}for (int a : {2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37}) {if (n == a)return true;if (check_composite(n, a, d, r))return false;}return true;}
+bool checksqr(ll k){return (trunc(sqrt(k))*trunc(sqrt(k))==k);}
+ll lcm(ll a, ll b){return (a*b)/gcd(a,b);}
+
+ll a[1000000],b[1000000],c[1000000];
+ii f[1000000];
+ii sol[1000000];
+map <ll,ll> z;
+signed main(){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+    //freopen("MONEY.inp","r",stdin);
+    //freopen("MONEY.out","w",stdout);
+    ll n,k;
+    cin >> n >> k;
+    For(i,1,n) cin >> a[i] >> b[i] >> c[i];
+    For(i,1,n){
+        f[i*2-1]={a[i],c[i]};
+        f[i*2]={b[i]+1,-c[i]};
+    }
+    sort(f+1,f+(n*2)+1);
+    ll kq=0;
+    ll t=f[1].y;
+    For(i,2,n*2){
+        kq+=min(t,k)*(f[i].x-f[i-1].x);
+        t+=f[i].y;
+    }
+    cout << kq;
+    return 0;
+}
+/*
+INPUT :
+
+OUTPUT :
+
+If it WA, check :
+- Special case (Usually, n=1)
+- WRONG FORMAT OUTPUT
+- Check reading
+- Change (ll) to (ull)
+- lleger Overflow (The number that bigger than 2^63-1)
+*/
