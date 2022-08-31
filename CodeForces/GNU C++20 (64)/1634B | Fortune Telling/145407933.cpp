da24623878dd5768b0cf154e7f9f64d777b738bf
trieu9797 <72329092+trieu9797@users.noreply.github.com>
2022-08-31 19:59:13 +0700

    Committing files

---
+++ CodeForces/GNU C++20 (64)/1634B | Fortune Telling/145407933.cpp
@@ -0,0 +1,40 @@
+/*
+Author : DeMen100ns (a.k.a Vo Khac Trieu self-destruct)
+School : VNU-HCM High school for the Gifted
+*/
+ 
+#include <bits/stdc++.h>
+ 
+#define int long long
+ 
+using namespace std;
+ 
+const int N = 1e5 + 5;
+const long long INF = 1e18 + 7;
+const int MAXA = 1e9;
+const int B = sqrt(N) + 5;
+ 
+int a[N];
+ 
+void solve(){
+ int n, x, y, cur;
+    cin >> n >> x >> y;
+    cur = x;
+    for(int i = 1; i <= n; ++i) cin >> a[i], cur += a[i];
+    if ((cur - y) % 2 == 0){
+        cout << "Alice" << endl;
+    } else cout << "Bob" << endl;
+}
+ 
+signed main(){
+    ios_base::sync_with_stdio(0); 
+    cin.tie(0); cout.tie(0);
+ 
+    //freopen("codeforces.inp","r",stdin);
+    //freopen("codeforces.out","w",stdout);
+    
+    int t = 1; cin >> t;
+    while(t--){
+        solve();
+    }
+}
