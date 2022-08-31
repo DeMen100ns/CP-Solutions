b88946d07b5edb8486080bbc0920ce6f7e40e1c4
trieu9797 <72329092+trieu9797@users.noreply.github.com>
2022-08-31 19:49:05 +0700

    Committing files

---
+++ CodeForces/GNU C++20 (64)/1716D | Chip Move/166995532.cpp
@@ -0,0 +1,70 @@
+/*
+Author : DeMen100ns (a.k.a Vo Khac Trieu)
+School : VNU-HCM High school for the Gifted
+fuck you adhoc
+ 
+Name: 
+Sauce: 
+Tag: 
+Sol: 
+Note: 
+*/
+ 
+#include <bits/stdc++.h>
+ 
+using namespace std;
+ 
+template<typename T> int SIZE(T (&t)){ return t.size(); } template<typename T, size_t N> int SIZE(T (&t)[N]){ return N; } string to_string(char t){ return "'" + string({t}) + "'"; } string to_string(bool t){ return t ? "true" : "false"; } string to_string(const string &t, int x1=0, int x2=1e9){ string ret = ""; for(int i = min(x1,SIZE(t)), _i = min(x2,SIZE(t)-1); i <= _i; ++i){ ret += t[i]; } return '"' + ret + '"'; } string to_string(const char* t){ string ret(t); return to_string(ret); } template<size_t N> string to_string(const bitset<N> &t, int x1=0, int x2=1e9){ string ret = ""; for(int i = min(x1,SIZE(t)); i <= min(x2,SIZE(t)-1); ++i){ ret += t[i] + '0'; } return to_string(ret); } template<typename T, typename... Coords> string to_string(const T (&t), int x1=0, int x2=1e9, Coords... C); template<typename T, typename S> string to_string(const pair<T, S> &t){ return "(" + to_string(t.first) + ", " + to_string(t.second) + ")"; } template<typename T, typename... Coords> string to_string(const T (&t), int x1, int x2, Coords... C){ string ret = "["; x1 = min(x1, SIZE(t)); auto e = begin(t); advance(e,x1); for(int i = x1, _i = min(x2,SIZE(t)-1); i <= _i; ++i){ ret += to_string(*e, C...) + (i != _i ? ", " : ""); e = next(e); } return ret + "]"; } template<int Index, typename... Ts> struct print_tuple{ string operator() (const tuple<Ts...>& t) { string ret = print_tuple<Index - 1, Ts...>{}(t); ret += (Index ? ", " : ""); return ret + to_string(get<Index>(t)); } }; template<typename... Ts> struct print_tuple<0, Ts...> { string operator() (const tuple<Ts...>& t) { return to_string(get<0>(t)); } }; template<typename... Ts> string to_string(const tuple<Ts...>& t) { const auto Size = tuple_size<tuple<Ts...>>::value; return print_tuple<Size - 1, Ts...>{}(t); } void dbgr(){;} template<typename Heads, typename... Tails> void dbgr(Heads H, Tails... T){ cout << to_string(H) << " | "; dbgr(T...); } void dbgs(){;} template<typename Heads, typename... Tails> void dbgs(Heads H, Tails... T){ cout << H << " "; dbgs(T...); } 
+#define dbgv(...) cout << to_string(__VA_ARGS__) << endl;
+#define dbg(...) cout << "[" << #__VA_ARGS__ << "]: "; dbgv(__VA_ARGS__);
+#define dbgr(...) dbgr(__VA_ARGS__); cout << endl;
+#define dbgm(...) cout << "[" << #__VA_ARGS__ << "]: "; dbgr(__VA_ARGS__);
+ 
+const int MOD = 998244353;
+void add(int &a, int b, int mod = MOD){ a += b; while (a < 0) a += mod; while (a >= mod) a -= mod; }
+int sum(int a, int b, int mod = MOD){ return add(a, b, mod), a; }
+void mul(int &a, int b, int mod = MOD){ a = (a * 1LL * b) % mod; }
+int prod(int a, int b, int mod = MOD){ return mul(a, b, mod), a; }
+int bpow(int a, int b, int mod = MOD){ int ans = 1; while (b){ if (b & 1){ mul(ans, a, mod); } mul(a, a, mod); b >>= 1; } return ans; }
+int Inv(int a, int mod = MOD){ return bpow(a, mod - 2, mod); }
+int Div(int a, int b, int mod = MOD) { return prod(a, Inv(b, mod), mod); }
+ 
+const int N = 2e5 + 5;
+const long long INF = numeric_limits<long long>::max() / 2;
+const int MAXA = 1e9;
+const int B = sqrt(N) + 5;
+ 
+int dpold[N], dpnew[N], ans[N];
+ 
+void solve()
+{
+    int n, k; cin >> n >> k;
+    dpold[0] = 1;
+    for(int s = k; s <= n; s += k, k++){
+        for(int i = k; i <= n; ++i){
+            dpnew[i] = sum(dpnew[i - k], dpold[i - k]);
+            add(ans[i], dpnew[i]);
+        }
+        for(int i = 0; i <= n; ++i){
+            dpold[i] = dpnew[i];
+            dpnew[i] = 0;
+        }
+    }
+    for(int i = 1; i <= n; ++i) cout << ans[i] << " ";
+}
+ 
+signed main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+ 
+    // freopen("codeforces.inp","r",stdin);
+    // freopen("codeforces.out","w",stdout);
+ 
+    int t = 1; //cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+}
