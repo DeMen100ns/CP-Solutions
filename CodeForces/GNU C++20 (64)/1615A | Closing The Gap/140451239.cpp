5ba549b30bf253772394ec247a5b63df494b18e5
trieu9797 <72329092+trieu9797@users.noreply.github.com>
2022-08-31 20:03:59 +0700

    Committing files

---
+++ CodeForces/GNU C++20 (64)/1615A | Closing The Gap/140451239.cpp
@@ -0,0 +1,35 @@
+/*
+Author : DeMen100ns (a.k.a Vo Khac Trieu self-destruct)
+School : VNU-HCM High school for the Gifted
+*/
+ 
+#include <bits/stdc++.h>
+ 
+using namespace std;
+ 
+const int N = 1e5 + 5;
+const long long INF = 1e18 + 7;
+const int MAXA = 1e9;
+const int B = sqrt(N) + 5;
+ 
+int a[N];
+ 
+void solve(){
+ int n, s = 0;
+    cin >> n;
+    for(int i = 1; i <= n; ++i) cin >> a[i], s += a[i];
+    cout << 1 - (s % n == 0) << endl;
+}
+ 
+signed main(){
+    ios_base::sync_with_stdio(0); 
+    cin.tie(0); cout.tie(0);
+ 
+    //freopen("codeforces.inp","r",stdin);
+    //freopen("codeforces.out","w",stdout);
+    
+    int t = 1; cin >> t;
+    while(t--){
+        solve();
+    }
+}
