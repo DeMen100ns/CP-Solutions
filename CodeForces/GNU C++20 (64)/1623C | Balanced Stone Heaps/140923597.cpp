0935dab1fef2db9a097791afe40bd7e3f4e2f255
trieu9797 <72329092+trieu9797@users.noreply.github.com>
2022-08-31 20:03:18 +0700

    Committing files

---
+++ CodeForces/GNU C++20 (64)/1623C | Balanced Stone Heaps/140923597.cpp
@@ -0,0 +1,68 @@
+/*
+Author : DeMen100ns (a.k.a Vo Khac Trieu self-destruct)
+School : VNU-HCM High school for the Gifted
+*/
+ 
+#include <bits/stdc++.h>
+ 
+#define ii pair<int, int>
+#define int long long
+#define x first
+#define y second
+ 
+using namespace std;
+ 
+template<typename T> int SIZE(T (&t)){ return t.size(); } template<typename T, size_t N> int SIZE(T (&t)[N]){ return N; } string to_string(char t){ return "'" + string({t}) + "'"; } string to_string(bool t){ return t ? "true" : "false"; } string to_string(const string &t, int x1=0, int x2=1e9){ string ret = ""; for(int i = min(x1,SIZE(t)), _i = min(x2,SIZE(t)-1); i <= _i; ++i){ ret += t[i]; } return '"' + ret + '"'; } string to_string(const char* t){ string ret(t); return to_string(ret); } template<size_t N> string to_string(const bitset<N> &t, int x1=0, int x2=1e9){ string ret = ""; for(int i = min(x1,SIZE(t)); i <= min(x2,SIZE(t)-1); ++i){ ret += t[i] + '0'; } return to_string(ret); } template<typename T, typename... Coords> string to_string(const T (&t), int x1=0, int x2=1e9, Coords... C); template<typename T, typename S> string to_string(const pair<T, S> &t){ return "(" + to_string(t.first) + ", " + to_string(t.second) + ")"; } template<typename T, typename... Coords> string to_string(const T (&t), int x1, int x2, Coords... C){ string ret = "["; x1 = min(x1, SIZE(t)); auto e = begin(t); advance(e,x1); for(int i = x1, _i = min(x2,SIZE(t)-1); i <= _i; ++i){ ret += to_string(*e, C...) + (i != _i ? ", " : ""); e = next(e); } return ret + "]"; } template<int Index, typename... Ts> struct print_tuple{ string operator() (const tuple<Ts...>& t) { string ret = print_tuple<Index - 1, Ts...>{}(t); ret += (Index ? ", " : ""); return ret + to_string(get<Index>(t)); } }; template<typename... Ts> struct print_tuple<0, Ts...> { string operator() (const tuple<Ts...>& t) { return to_string(get<0>(t)); } }; template<typename... Ts> string to_string(const tuple<Ts...>& t) { const auto Size = tuple_size<tuple<Ts...>>::value; return print_tuple<Size - 1, Ts...>{}(t); } void dbgr(){;} template<typename Heads, typename... Tails> void dbgr(Heads H, Tails... T){ cout << to_string(H) << " | "; dbgr(T...); } void dbgs(){;} template<typename Heads, typename... Tails> void dbgs(Heads H, Tails... T){ cout << H << " "; dbgs(T...); } 
+#define dbgv(...) cout << to_string(__VA_ARGS__) << endl;
+#define dbg(...) cout << "[" << #__VA_ARGS__ << "]: "; dbgv(__VA_ARGS__);
+#define dbgr(...) dbgr(__VA_ARGS__); cout << endl;
+#define dbgm(...) cout << "[" << #__VA_ARGS__ << "]: "; dbgr(__VA_ARGS__);
+ 
+const int N = 2e5 + 5;
+const long long INF = 1e18 + 7;
+const int MAXA = 1e9;
+const int B = sqrt(N) + 5;
+ 
+int a[N], b[N], c[N];
+ 
+void solve(){
+    int n;
+    cin >> n;
+    for(int i = 1; i <= n; ++i) cin >> a[i], b[i] = a[i];
+    int l = 0, r = 1e9 + 1;
+    while (l + 1 < r){
+        int mid = (l + r) >> 1;
+        //dbg(mid);
+        for(int i = 1; i <= n; ++i) a[i] = b[i], c[i] = 0;
+        for(int i = n; i >= 3; --i){
+            if (a[i] + c[i] < mid) continue;
+            int k = min(a[i], (a[i] + c[i] - mid)) / 3;
+            c[i] -= k * 3;
+            c[i - 1] += k;
+            c[i - 2] += 2 * k;
+            //dbg(i);
+            //dbg(a, 1, n);
+            //dbg(c, 1, n);
+        }
+        bool f = true;
+        for(int i = 1; i <= n; ++i){
+            f &= (a[i] + c[i] >= mid);
+        }
+        if (f) l = mid;
+        else r = mid;
+    }
+    cout << l << endl;
+}
+ 
+signed main(){
+    ios_base::sync_with_stdio(0); 
+    cin.tie(0); cout.tie(0);
+ 
+    //freopen("codeforces.inp","r",stdin);
+    //freopen("codeforces.out","w",stdout);
+    
+    int t = 1; cin >> t;
+    while(t--){
+        solve();
+    }
+}
