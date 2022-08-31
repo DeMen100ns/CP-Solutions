be5a48ca8fb4aaa387425ac1d1c5a81e76b92308
trieu9797 <72329092+trieu9797@users.noreply.github.com>
2022-08-31 19:51:22 +0700

    Committing files

---
+++ CodeForces/GNU C++20 (64)/1694A | Creep/160824794.cpp
@@ -0,0 +1,46 @@
+/*
+Author : DeMen100ns (a.k.a Vo Khac Trieu)
+School : VNU-HCM High school for the Gifted
+fuck you adhoc
+*/
+ 
+#include <bits/stdc++.h>
+ 
+using namespace std;
+ 
+const int N = 2e5 + 5;
+const long long INF = 1e18 + 7;
+const int MAXA = 1e9;
+const int B = sqrt(N) + 5;
+ 
+void solve()
+{
+    int a, b; cin >> a >> b;
+    int mi = min(a, b);
+    for(int i = 1; i <= mi; ++i){
+        cout << 10;
+    }
+    int cur = 0;
+    if (a < b){
+        cur = 1;
+    }
+    for(int i = mi + 1; i <= max(a, b); ++i){
+        cout << cur;
+    } cout << endl;
+}
+ 
+signed main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+ 
+    // freopen("codeforces.inp","r",stdin);
+    // freopen("codeforces.out","w",stdout);
+ 
+    int t = 1; cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+}
