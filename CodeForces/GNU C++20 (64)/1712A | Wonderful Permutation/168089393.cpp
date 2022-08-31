278b7ccb6949799cdac3ab8ab10a951bc6bc5856
trieu9797 <72329092+trieu9797@users.noreply.github.com>
2022-08-31 19:48:40 +0700

    Committing files

---
+++ CodeForces/GNU C++20 (64)/1712A | Wonderful Permutation/168089393.cpp
@@ -0,0 +1,49 @@
+/*
+Author : DeMen100ns (a.k.a Vo Khac Trieu)
+School : VNU-HCM High school for the Gifted
+fuck you adhoc
+ 
+Name: 
+Sauce: 
+Tag: 
+Sol: 
+Note: 
+*/
+ 
+#include <bits/stdc++.h>
+ 
+using namespace std;
+ 
+const int N = 2e5 + 5;
+const long long INF = numeric_limits<long long>::max() / 2;
+const int MAXA = 1e9;
+const int B = sqrt(N) + 5;
+ 
+int a[N];
+ 
+void solve()
+{
+    int n, k; cin >> n >> k;
+    for(int i = 1; i <= n; ++i) cin >> a[i];
+    int cnt = k;
+    for(int i = 1; i <= k; ++i){
+        cnt -= (a[i] <= k);
+    }
+    cout << cnt << endl;
+}
+ 
+signed main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+ 
+    // freopen("codeforces.inp","r",stdin);
+    // freopen("codeforces.out","w",stdout);
+ 
+    int t = 1; cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+}
