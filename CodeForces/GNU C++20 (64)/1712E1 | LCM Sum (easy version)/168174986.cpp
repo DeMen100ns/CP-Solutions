18eb16072921e97be02f2542428ceb2a0fef13c6
trieu9797 <72329092+trieu9797@users.noreply.github.com>
2022-08-31 19:47:47 +0700

    Committing files

---
+++ CodeForces/GNU C++20 (64)/1712E1 | LCM Sum (easy version)/168174986.cpp
@@ -0,0 +1,81 @@
+/*
+Author : DeMen100ns (a.k.a Vo Khac Trieu)
+School : VNU-HCM High school for the Gifted
+fuck you adhoc
+ 
+Name:
+Sauce: 
+Tag: 
+Sol: 
+Note: 
+*/
+ 
+#include <bits/stdc++.h>
+ 
+using namespace std;
+ 
+template<typename T> int SIZE(T (&t)){ return t.size(); } template<typename T, size_t N> int SIZE(T (&t)[N]){ return N; } string to_string(char t){ return "'" + string({t}) + "'"; } string to_string(bool t){ return t ? "true" : "false"; } string to_string(const string &t, int x1=0, int x2=1e9){ string ret = ""; for(int i = min(x1,SIZE(t)), _i = min(x2,SIZE(t)-1); i <= _i; ++i){ ret += t[i]; } return '"' + ret + '"'; } string to_string(const char* t){ string ret(t); return to_string(ret); } template<size_t N> string to_string(const bitset<N> &t, int x1=0, int x2=1e9){ string ret = ""; for(int i = min(x1,SIZE(t)); i <= min(x2,SIZE(t)-1); ++i){ ret += t[i] + '0'; } return to_string(ret); } template<typename T, typename... Coords> string to_string(const T (&t), int x1=0, int x2=1e9, Coords... C); template<typename T, typename S> string to_string(const pair<T, S> &t){ return "(" + to_string(t.first) + ", " + to_string(t.second) + ")"; } template<typename T, typename... Coords> string to_string(const T (&t), int x1, int x2, Coords... C){ string ret = "["; x1 = min(x1, SIZE(t)); auto e = begin(t); advance(e,x1); for(int i = x1, _i = min(x2,SIZE(t)-1); i <= _i; ++i){ ret += to_string(*e, C...) + (i != _i ? ", " : ""); e = next(e); } return ret + "]"; } template<int Index, typename... Ts> struct print_tuple{ string operator() (const tuple<Ts...>& t) { string ret = print_tuple<Index - 1, Ts...>{}(t); ret += (Index ? ", " : ""); return ret + to_string(get<Index>(t)); } }; template<typename... Ts> struct print_tuple<0, Ts...> { string operator() (const tuple<Ts...>& t) { return to_string(get<0>(t)); } }; template<typename... Ts> string to_string(const tuple<Ts...>& t) { const auto Size = tuple_size<tuple<Ts...>>::value; return print_tuple<Size - 1, Ts...>{}(t); } void dbgr(){;} template<typename Heads, typename... Tails> void dbgr(Heads H, Tails... T){ cout << to_string(H) << " | "; dbgr(T...); } void dbgs(){;} template<typename Heads, typename... Tails> void dbgs(Heads H, Tails... T){ cout << H << " "; dbgs(T...); } 
+#define dbgv(...) cout << to_string(__VA_ARGS__) << endl;
+#define dbg(...) cout << "[" << #__VA_ARGS__ << "]: "; dbgv(__VA_ARGS__);
+#define dbgr(...) dbgr(__VA_ARGS__); cout << endl;
+#define dbgm(...) cout << "[" << #__VA_ARGS__ << "]: "; dbgr(__VA_ARGS__);
+ 
+const int N = 2e5 + 5;
+const long long INF = 1e18 + 7;
+const int MAXA = 4e5;
+const int B = sqrt(N) + 5;
+ 
+vector <int> u[2 * N];
+ 
+int lcm(int a, int b){
+    return a / __gcd(a, b) * b;
+}
+ 
+void precal(){
+    for(int i = 1; i <= MAXA; ++i){
+        for(int j = 1; j * j <= i; ++j){
+            if (i % j == 0){
+                u[i].push_back(j);
+                if (j * j < i) u[i].push_back(i / j);
+            }
+        }
+    }
+}
+ 
+void solve()
+{
+    int l, r; long long ans; cin >> l >> r;
+    ans = (r - l + 1);
+    ans = ans * (ans - 1) * (ans - 2) / 6;
+    for(int k = l; k <= r; ++k){
+        for(int i = 0; i < (int)u[2 * k].size(); ++i){
+            int I = u[2 * k][i];
+            if (I < l || I >= k) continue;
+            int tmp = lcm(I, k);
+            for(int j = i + 1; j < (int)u[2 * k].size(); ++j){
+                int J = u[2 * k][j];
+                if (J < l || J >= k) continue;
+                
+                if (lcm(tmp, J) < I + J + k) --ans;
+            }
+        }
+    }
+    cout << ans << endl;
+}
+ 
+signed main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+ 
+    // freopen("codeforces.inp","r",stdin);
+    // freopen("codeforces.out","w",stdout);
+ 
+    precal();
+    int t = 1; cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+}
