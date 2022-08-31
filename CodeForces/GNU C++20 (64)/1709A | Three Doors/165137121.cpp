dba1c310e14d0d18b251256bfbe3cef623a53737
trieu9797 <72329092+trieu9797@users.noreply.github.com>
2022-08-31 19:50:01 +0700

    Committing files

---
+++ CodeForces/GNU C++20 (64)/1709A | Three Doors/165137121.cpp
@@ -0,0 +1,43 @@
+/*
+Author : DeMen100ns (a.k.a Vo Khac Trieu)
+School : VNU-HCM High school for the Gifted
+fuck you adhoc
+*/
+ 
+#include <bits/stdc++.h>
+ 
+using namespace std;
+ 
+const int N = 2e5 + 5;
+const long long INF = 1e18 + 7;
+const int MAXA = 1e9;
+const int B = sqrt(N) + 5;
+ 
+int a[4];
+ 
+void solve()
+{
+    int x;
+    cin >> x >> a[1] >> a[2] >> a[3];
+    
+    for(int i = 1; i <= 2; ++i) x = a[x];
+    if (x == 0) cout << "NO";
+    else cout << "YES";
+    cout << endl;
+}
+ 
+signed main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+ 
+    // freopen("codeforces.inp","r",stdin);
+    // freopen("codeforces.out","w",stdout);
+ 
+    int t = 1; cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+}
