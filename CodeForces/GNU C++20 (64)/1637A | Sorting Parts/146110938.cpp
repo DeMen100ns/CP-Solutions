67edb70e96be7366260a1b0d37e66409a1ff0656
trieu9797 <72329092+trieu9797@users.noreply.github.com>
2022-08-31 19:58:41 +0700

    Committing files

---
+++ CodeForces/GNU C++20 (64)/1637A | Sorting Parts/146110938.cpp
@@ -0,0 +1,43 @@
+/*
+Author : DeMen100ns (a.k.a Vo Khac Trieu self-destruct)
+School : VNU-HCM High school for the Gifted
+*/
+ 
+#include <bits/stdc++.h>
+ 
+using namespace std;
+ 
+const int N = 1e5 + 5;
+const long long INF = 1e18 + 7;
+const int MAXA = 1e9;
+const int B = sqrt(N) + 5;
+ 
+int a[N];
+ 
+void solve(){
+    int n;
+    cin >> n;
+    bool f = true;
+    for(int i = 1; i <= n; ++i){
+     cin >> a[i];
+     f &= (a[i] >= a[i - 1]);
+    }
+    if (f) cout << "NO";
+    else cout << "yes";
+    cout << endl;
+ 
+ 
+}
+ 
+signed main(){
+    ios_base::sync_with_stdio(0); 
+    cin.tie(0); cout.tie(0);
+ 
+    //freopen("codeforces.inp","r",stdin);
+    //freopen("codeforces.out","w",stdout);
+    
+    int t = 1; cin >> t;
+    while(t--){
+        solve();
+    }
+}
