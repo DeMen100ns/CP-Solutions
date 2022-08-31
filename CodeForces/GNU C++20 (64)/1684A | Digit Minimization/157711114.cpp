8136125b4685eba8ebd3f5e3c2700d1f6a7490cd
trieu9797 <72329092+trieu9797@users.noreply.github.com>
2022-08-31 19:53:14 +0700

    Committing files

---
+++ CodeForces/GNU C++20 (64)/1684A | Digit Minimization/157711114.cpp
@@ -0,0 +1,41 @@
+/*
+Author : DeMen100ns (a.k.a Vo Khac Trieu)
+School : VNU-HCM High school for the Gifted
+fuck you adhoc
+*/
+ 
+#include <bits/stdc++.h>
+ 
+using namespace std;
+ 
+const int N = 2e5 + 5;
+const long long INF = 1e18 + 7;
+const int MAXA = 1e9;
+const int B = sqrt(N) + 5;
+ 
+void solve()
+{
+    string s; cin >> s;
+    if (s.size() == 2){
+        cout << s[1] << endl;
+    } else {
+        sort(s.begin(), s.end());
+        cout << s[0] << endl;
+    }
+}
+ 
+signed main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+ 
+    // freopen("codeforces.inp","r",stdin);
+    // freopen("codeforces.out","w",stdout);
+ 
+    int t = 1; cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+}
