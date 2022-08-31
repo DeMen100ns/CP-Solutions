a666f17e6aed3910d7f06fac9bd8179366cb28cb
trieu9797 <72329092+trieu9797@users.noreply.github.com>
2022-08-31 20:06:12 +0700

    Committing files

---
+++ CodeForces/GNU C++20 (64)/1555E | Boring Segments/137330501.cpp
@@ -0,0 +1,151 @@
+//Author : Trieu self-destruct
+//1st O30/4
+//TST next year
+ 
+#include <bits/stdc++.h>
+//#include <ext/pb_ds/assoc_container.hpp>
+//#include <ext/pb_ds/tree_policy.hpp>
+ 
+//using namespace __gnu_pbds;
+ 
+using namespace std;
+ 
+/*Pragma*/
+//#pragma GCC optimize("Ofast")
+//#pragma GCC optimize("O3")
+//#pragma GCC target("avx,avx2,fma")
+//#pragma GCC optimization ("unroll-loops")
+//#pragma GCC optimize("Os")
+ 
+/*Weird define*/
+#define ll long long
+#define endl '\n'
+#define gcd __gcd
+#define count1 __builtin_popcount
+//#define ordered_set tree<int, null_type,less<int>, rb_tree_tag,tree_order_statistics_node_update>
+//#define p prev
+ 
+/*For define*/
+#define For(i,a,b) for(ll i=a;i<=b;i++)
+#define foR(i,a,b) for(ll i=a;i>=b;i--)
+#define FOR(i,a,b) for(ll i=a;i<b;i++)
+#define forN(i,a,b,c) for(ll i=a;i<=b;i+=c)
+#define foRN(i,a,b,c) for(ll i=a;i>=b;i-=c)
+#define fora(i, a) for (auto i : a)
+ 
+/*push-pop-pair define*/
+#define pb push_back
+#define pob pop_back
+#define pf push_front
+#define pof pop_front
+#define x first
+#define y second
+ 
+/*Print define*/
+#define write(a, l, r) for (ll ik = l; ik <= r; ik++) cout << a[ik] << ' '; cout << endl
+#define writeln(a, l, r) for (ll ik = l; ik <= r; ik++) cout << a[ik] << endl
+ 
+/*Type-def*/
+typedef unsigned long long  ull;
+typedef vector<ll>   vi;
+typedef pair<int, int>  ii;
+typedef long double         ld;
+ 
+/*some const*/
+const ll IMA = LLONG_MAX;
+const ll IMI = LLONG_MIN;
+const ll INF = 1e15;
+const ll MOD = 1000000007;
+ 
+/* Calculation with MODULO*/
+ll dec(ll x, ll y) {return (x-y+MOD)%MOD;}
+ll add(ll x, ll y) {return (x + y) % MOD;}
+ll mul(ll x, ll y) {return ((x%MOD) * (y%MOD)) % MOD;}
+ll bpow(ll x, ll y) {ll res = 1; while (y) {if (y & 1) res = mul(res, x); x = mul(x, x); y >>= 1;} return res;}
+ll ModInverse(ll x) {return bpow(x, MOD - 2);}
+ll Div(ll x, ll y) {return mul(x, ModInverse(y));}
+ll GetBit(ll val, ll num) {return ((val>>num) & 1LL);}
+ 
+/*Some simple checking and calculation function*/
+string yn(bool k){if (k) return "YES"; else return "NO";}
+ll binpower(ll base, ll e, ll mod) {ll result = 1;base %= mod;while (e){if (e & 1)result = (ll)result * base % mod;base = (ll)base * base % mod;e >>= 1;    }return result;}
+bool check_composite(long long n, long long a, long long d, int s) {long long x = binpower(a, d, n); if (x == 1 || x == n - 1)return false;for (int r = 1; r < s; r++) {x = (long long)x * x % n;if (x == n - 1)return false;}return true;};
+bool checkprime(long long n) {if (n < 2)return false;int r = 0;long long d = n - 1;while ((d & 1) == 0) {d >>= 1;r++;}for (int a : {2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37}) {if (n == a)return true;if (check_composite(n, a, d, r))return false;}return true;}
+bool checksqr(ll k){return (trunc(sqrt(k))*trunc(sqrt(k))==k);}
+ll lcm(ll a, ll b){return (a*b)/gcd(a,b);}
+ 
+const int N=1e6+5;
+const int M=1e6+5;
+pair <int,ii> a[N];
+int seg[4*M], f[4*M];
+ 
+void upd(ll id, ll l, ll r, ll u, ll v, ll val) {
+    if (v < l || r < u) {
+        return ;
+    }
+    if (u <= l && r <= v) {
+        f[id]+=val;
+        if (f[id]==1) seg[id]=(r-l+1);
+        if (!f[id]){
+            if (l<r) seg[id]=seg[id<<1]+seg[id<<1|1];
+            else seg[id]=0;
+        }
+        return;
+    }
+    ll mid = (l + r) >>1;
+ 
+    upd(id<< 1, l, mid, u, v, val);
+    upd(id<< 1 | 1, mid+1, r, u, v, val);
+ 
+    if (!f[id] && l<r) seg[id] = (seg[id<< 1] + seg[id<< 1 | 1]);
+}
+ 
+signed main(){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+    //freopen("LCSUB.inp","r",stdin);
+    //freopen("LCSUB.out","w",stdout);
+    int n,m;
+    cin >> n >> m;
+    For(i,1,n){
+        cin >> a[i].y.x >> a[i].y.y >> a[i].x;
+        a[i].y.y--;
+    }
+    sort(a+1,a+n+1);
+    int mi=a[n].x-a[1].x;
+    int j=1;
+    bool f=false;
+    For(i,1,n){
+        while (j<=n && !f){
+            upd(1,1,m,a[j].y.x,a[j].y.y,1);
+            j++;
+            if (seg[1]==m-1) f=true;
+        }
+        //cout << i << " " << j << " " << seg[1] << endl;
+        if (seg[1]==m-1) mi=min(mi,a[j-1].x-a[i].x);
+        //cout << a[i].x << endl;
+        upd(1,1,m,a[i].y.x,a[i].y.y,-1);
+        if (seg[1]<m-1) f=false;
+    }
+    cout << mi;
+    return 0;
+}
+/*
+23072007
+//Delete define endl when interactive prolem
+ 
+INPUT :
+4
+3 5
+4 6
+3 2
+69 696
+OUTPUT :
+ 
+If it WA, check :
+- Special case (Usually, n=1)
+- WRONG FORMAT OUTPUT
+- Check reading
+- Change (ll) to (ull)
+- lleger Overflow (The number that bigger than 2^63-1)
+*/
