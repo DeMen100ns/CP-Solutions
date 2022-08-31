a35277a8d6713ef9cbb34eda81b6e8a4d0034603
trieu9797 <72329092+trieu9797@users.noreply.github.com>
2022-08-31 19:42:43 +0700

    Committing files

---
+++ Atcoder/C++ (GCC 9.2.1)/abc153_d/33164201.cpp
@@ -0,0 +1,41 @@
+/*
+Author : DeMen100ns (a.k.a Vo Khac Trieu)
+School : VNU-HCM High school for the Gifted
+fuck you adhoc
+*/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+const int N = 2e5 + 5;
+const long long INF = numeric_limits<long long>::max() / 2;
+const int MAXA = 1e9;
+const int B = sqrt(N) + 5;
+
+void solve()
+{
+    long long n, ans = 0, cnt = 1; cin >> n;
+    while (n){
+        ans += cnt;
+        n >>= 1;
+        cnt <<= 1;
+    }
+    cout << ans;
+}
+
+signed main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    // freopen("codeforces.inp","r",stdin);
+    // freopen("codeforces.out","w",stdout);
+
+    int t = 1; //cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+}
