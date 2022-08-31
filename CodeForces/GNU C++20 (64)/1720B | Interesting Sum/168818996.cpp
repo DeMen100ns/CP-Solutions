4cf2363a470318c2241e4e57ded3cd18bcebd961
trieu9797 <72329092+trieu9797@users.noreply.github.com>
2022-08-31 19:47:00 +0700

    Committing files

---
+++ CodeForces/GNU C++20 (64)/1720B | Interesting Sum/168818996.cpp
@@ -0,0 +1,46 @@
+/*
+Author : DeMen100ns (a.k.a Vo Khac Trieu)
+School : VNU-HCM High school for the Gifted
+fuck you adhoc
+ 
+Name:
+Sauce: 
+Tag: 
+Sol: 
+Note: 
+*/
+ 
+#include <bits/stdc++.h>
+ 
+using namespace std;
+ 
+const int N = 2e5 + 5;
+const long long INF = 1e18 + 7;
+const int MAXA = 1e9;
+const int B = sqrt(N) + 5;
+ 
+int a[N];
+ 
+void solve()
+{
+    int n; cin >> n;
+    for(int i = 1; i <= n; ++i) cin >> a[i];
+    sort(a + 1, a + n + 1);
+    cout << (a[n - 1] + a[n]) - (a[1] + a[2]) << endl;
+}
+ 
+signed main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+ 
+    // freopen("codeforces.inp","r",stdin);
+    // freopen("codeforces.out","w",stdout);
+ 
+    int t = 1; cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+}
