a74a2a44319f1df49ce555f1a3d5c59815d4297b
trieu9797 <72329092+trieu9797@users.noreply.github.com>
2022-08-31 19:43:25 +0700

    Committing files

---
+++ Atcoder/C++ (GCC 9.2.1)/arc135_a/31907145.cpp
@@ -0,0 +1,55 @@
+/*
+Author : DeMen100ns (a.k.a Vo Khac Trieu)
+School : VNU-HCM High school for the Gifted
+fuck you adhoc
+*/
+
+#include <bits/stdc++.h>
+
+#define int long long
+
+using namespace std;
+
+const int MOD = 998244353;
+inline void add(int &x, int y, int mod = MOD) { x += y; while (x >= mod) x -= mod; while (x < 0) x += mod;}
+inline void mul(int &x, int y, int mod = MOD) { x = (x * 1LL * y) % mod;}
+inline int prod(int x, int y, int mod = MOD) { return mul(x, y, mod), x;}
+inline int sum(int x, int y, int mod = MOD) { return add(x, y, mod), x;}
+inline int bpow(int x, int y, int mod = MOD) { int ans = 1; while (y) { if (y & 1) mul(ans, x, mod); mul(x, x, mod); y >>= 1;} return ans;}
+inline int Inv(int x, int mod = MOD) { return bpow(x, mod - 2, mod);}
+inline int Div(int x, int y, int mod = MOD) { return prod(x, Inv(y, mod), mod);}
+
+const int N = 2e5 + 5;
+const long long INF = 1e18 + 7;
+const int MAXA = 1e9;
+const int B = sqrt(N) + 5;
+
+map<int, int> dp;
+
+int cal(int x){
+    if (dp.find(x) != dp.end()) return dp[x];
+    if (x <= 4) return x;
+    else return dp[x] = prod(cal(x / 2), cal((x + 1) / 2));
+}
+
+void solve()
+{
+    int x; cin >> x;
+    cout << cal(x);
+}
+
+signed main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    // freopen("codeforces.inp","r",stdin);
+    // freopen("codeforces.out","w",stdout);
+
+    int t = 1; // cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+}
