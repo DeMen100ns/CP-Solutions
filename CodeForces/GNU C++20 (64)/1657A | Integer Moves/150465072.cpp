1fc930b2635634b6acb1a0c4a8c3941ac8a119e6
trieu9797 <72329092+trieu9797@users.noreply.github.com>
2022-08-31 19:56:59 +0700

    Committing files

---
+++ CodeForces/GNU C++20 (64)/1657A | Integer Moves/150465072.cpp
@@ -0,0 +1,42 @@
+/*
+Author : DeMen100ns (a.k.a Vo Khac Trieu self-destruct)
+School : VNU-HCM High school for the Gifted
+*/
+ 
+#include <bits/stdc++.h>
+ 
+using namespace std;
+ 
+const int N = 2e5 + 5;
+const long long INF = 1e18 + 7;
+const int MAXA = 1e9;
+const int B = sqrt(N) + 5;
+ 
+void solve(){
+    int x, y;
+    cin >> x >> y;
+    if (x == 0 && y == 0){
+     cout << 0 << endl;
+     return;
+    }
+    int k = sqrt(x * x + y * y);
+    if (k * k == x * x + y * y){
+     cout << 1 << endl;
+     return;
+    }
+    cout << 2 << endl;
+}
+ 
+signed main(){
+    ios_base::sync_with_stdio(0); 
+    cin.tie(0); cout.tie(0);
+ 
+    //freopen("codeforces.inp","r",stdin);
+    //freopen("codeforces.out","w",stdout);
+    
+    int t = 1; cin >> t;
+    while(t--){
+        solve();
+    }
+    cerr << endl << "Time elapsed: " << 1.0 * clock() / CLOCKS_PER_SEC << " s.\n";
+}
