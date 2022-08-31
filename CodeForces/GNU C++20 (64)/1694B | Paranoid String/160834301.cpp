a1887c088ec1f9b9d27ef9cb5d727ec951dce085
trieu9797 <72329092+trieu9797@users.noreply.github.com>
2022-08-31 19:51:19 +0700

    Committing files

---
+++ CodeForces/GNU C++20 (64)/1694B | Paranoid String/160834301.cpp
@@ -0,0 +1,45 @@
+/*
+Author : DeMen100ns (a.k.a Vo Khac Trieu)
+School : VNU-HCM High school for the Gifted
+fuck you adhoc
+*/
+ 
+#include <bits/stdc++.h>
+#define int long long
+ 
+using namespace std;
+ 
+const int N = 2e5 + 5;
+const long long INF = 1e18 + 7;
+const int MAXA = 1e9;
+const int B = sqrt(N) + 5;
+ 
+void solve()
+{
+    int n; cin >> n;
+    string s; cin >> s;
+    int ans = 1;
+    for(int i = 1; i < n; ++i){
+        if (s[i - 1] == s[i]){
+            ans++; continue;
+        }
+        ans += (i + 1);
+    }
+    cout << ans << endl;
+}
+ 
+signed main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+ 
+    // freopen("codeforces.inp","r",stdin);
+    // freopen("codeforces.out","w",stdout);
+ 
+    int t = 1; cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+}
